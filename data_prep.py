955fb64175788cdf7d4fae17a68150d3657a7e3e
Devon Akers <devon.p.akers@vanderbilt.edu>
2017-12-16 15:21:55 -0500

    generating vocab from all data files

--- data_prep.py
+++ data_prep.py
@@ -9,25 +9,29 @@ def alphanumerical(text):
     return re.sub(pattern="^[a-zA-Z0-9\s]", repl="", string=text)
 
 
-def build_corpus(folders):
+def build_corpus():
     corpus = []
-    for folder in folders:
-        print("Folder: %s" % folder)
+    for folder in data_folders():
         for file in os.listdir(folder):
-            print("File: %s" % file)
             file = open("%s/%s" % (folder, file))
             corpus.append(alphanumerical("".join(file.readlines())))
             file.close()
     return corpus
 
 
+def data_folders():
+    folders = []
+    base_dir = 'data_backup/seeking_alpha'
+    for folder in os.listdir(base_dir):
+        folders.append("%s/%s" % (base_dir, folder))
+    return folders
+
 stopwords = stopwords.words('english')
 test_file = open('/Users/devonakers/udacity/capstone/data_backup/seeking_alpha/A/A_August 15, 2017 04:30 PM ET', 'r')
 test_transcript = alphanumerical("".join(test_file.readlines()))
-corpus = build_corpus(['/Users/devonakers/udacity/capstone/data_backup/seeking_alpha/A'])
+corpus = build_corpus()
 
 print("Fitting CountVectorizer to determine vocabulary.")
-print("Corpus: %s" % corpus)
 cnt_vec = CountVectorizer(input=test_transcript, stop_words=set(stopwords)).fit(corpus)
 print("Final vocabulary:")
 for v in cnt_vec.vocabulary_:
