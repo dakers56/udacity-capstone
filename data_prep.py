5600c195c5c5a4c04284ed65129d79cc515bec45
Devon Akers <devon.p.akers@vanderbilt.edu>
2017-12-16 16:17:40 -0500

    refactored write message

--- data_prep.py
+++ data_prep.py
@@ -7,8 +7,10 @@ import datetime
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 def alphanumerical(text):
     return re.sub(pattern="^[a-zA-Z0-9\s]", repl="", string=text)
+
 
 def build_corpus():
     corpus = []
@@ -19,12 +21,19 @@ def build_corpus():
             file.close()
     return corpus
 
+
 def data_folders():
     folders = []
     base_dir = 'data_backup/seeking_alpha'
     for folder in os.listdir(base_dir):
         folders.append("%s/%s" % (base_dir, folder))
     return folders
+
+
+def write(cnt_vec, folder='./'):
+    now = str(dateutil.parser.parse(str(datetime.datetime.now()))).replace(" ", "_")
+    joblib.dump(cnt_vec, "%svocab_%s.pkl" % (folder, now))
+
 
 stopwords = stopwords.words('english')
 test_file = open('/Users/devonakers/udacity/capstone/data_backup/seeking_alpha/A/A_August 15, 2017 04:30 PM ET', 'r')
@@ -33,10 +42,9 @@ corpus = build_corpus()
 
 print("Fitting CountVectorizer to determine vocabulary.")
 cnt_vec = CountVectorizer(input=test_transcript, stop_words=set(stopwords)).fit(corpus)
+cnt_vec = '{}'
 print("Final vocabulary:")
 for v in cnt_vec.vocabulary_:
     print(str(v))
 
-now = str(dateutil.parser.parse(str(datetime.datetime.now()))).replace(" ", "_")
-print("now: %s" % now)
-joblib.dump(cnt_vec, "vocab_%s.pkl" % now)
+write(cnt_vec)
