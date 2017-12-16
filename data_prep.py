89de048a2f667e8290bc7aa206c78b6f5bb22ff8
Devon Akers <devon.p.akers@vanderbilt.edu>
2017-12-16 15:07:16 -0500

    generate corpus from multiple files

--- data_prep.py
+++ data_prep.py
@@ -1,4 +1,5 @@
 import re
+import os
 
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
@@ -8,10 +9,22 @@ def alphanumerical(text):
     return re.sub(pattern="^[a-zA-Z0-9\s]", repl="", string=text)
 
 
+def build_corpus(folders):
+    corpus = []
+    for folder in folders:
+        print("Folder: %s" % folder)
+        for file in os.listdir(folder):
+            print("File: %s" % file)
+            file = open("%s/%s" % (folder, file))
+            corpus.append(alphanumerical("".join(file.readlines())))
+            file.close()
+    return corpus
+
+
 stopwords = stopwords.words('english')
 test_file = open('/Users/devonakers/udacity/capstone/data_backup/seeking_alpha/A/A_August 15, 2017 04:30 PM ET', 'r')
 test_transcript = alphanumerical("".join(test_file.readlines()))
-corpus = [test_transcript]
+corpus = build_corpus(['/Users/devonakers/udacity/capstone/data_backup/seeking_alpha/A'])
 
 print("Fitting CountVectorizer to determine vocabulary.")
 print("Corpus: %s" % corpus)
