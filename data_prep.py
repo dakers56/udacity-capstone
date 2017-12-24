1df21050a89e400b91310945ee717d50fcac0505
Devon Akers <devon.p.akers@vanderbilt.edu>
2017-12-24 13:32:44 -0500

    testing why X_train is an np.array

--- data_prep.py
+++ data_prep.py
@@ -195,7 +195,7 @@ def get_input_data(cnt_vec, base_dir='data_backup/seeking_alpha'):
                 try:
                     print("all_diluted_eps: %s" % all_diluted_eps_)
                     print("diluted_eps: %s" % diluted_eps)
-                    all_diluted_eps.append(np.array([diluted_eps]))
+                    all_diluted_eps_.append(np.array([diluted_eps]))
                 except AttributeError:
                     print("Shape of all_diluted_eps: %s" % str(all_diluted_eps.shape))
                     print("Shape of all_diluted_eps: %s" % str(np.array([diluted_eps]).shape))
