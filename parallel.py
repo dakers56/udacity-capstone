7eac6addd77e78b45138f706c968a7de944f6603
Ubuntu <ubuntu@ip-10-206-47-85.dqa.capitalone.com>
2018-01-24 20:34:48 +0000

    demo parallel capability

---
+++ parallel.py
@@ -0,0 +1,38 @@
+# Trying to parallelize stemming
+import threading
+from nltk.stem import PorterStemmer
+from multiprocessing import Pool
+
+files = None
+num_cores = 64
+# ps = [PorterStemmer() for i in range(num_cores)]
+
+
+
+
+def test(i):
+    print("Running test thread %s" % str(i))
+    print("Exiting test thread %s" % str(i))
+    return i
+
+
+def partition(data):
+    l = len(data)
+    print('l: %s' % l)
+    n_parts = int(l / num_cores)
+    print('n_parts: %s' % n_parts)
+    size_part = int(l / n_parts)
+    print('size_part: %s' % size_part)
+    parts = [data[i * size_part: (i + 1) * size_part] for i in range(n_parts)]
+    parts.append(data[n_parts * size_part:])
+    return parts
+
+
+def parallelize(func, iter):
+    pool = Pool()
+    return pool.map(func, iter)
+
+
+if __name__ == '__main__':
+   data = ["thread_%s" % i for i in range(512)]
+   print(partition(data))
