c8137bb1016d8c4f3a98286c41decfc6e0b6423e
Ubuntu <ubuntu@ip-10-206-46-177.dqa.capitalone.com>
2018-04-17 17:22:11 +0000

    added script for initial setup

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+aws s3 cp s3://nebula-poc/enamel/Anaconda3-4.3.0-Linux-x86_64.sh /home/ubuntu
+bash /home/ubuntu/Anaconda3-4.3.0-Linux-x86_64.sh -b
+echo "export PATH=/home/ubuntu/anaconda3/bin:$PATH" >> /home/ubuntu/.bash_profile
+
+source /home/ubuntu/.bash_profile
+conda create -n udacity_ml
